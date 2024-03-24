20cbee10b20bfd2c7d60f9ba85759ca6945fd47e
Kevin <113167092+Lagergrenk@users.noreply.github.com>
2024-03-24 14:18:28 +0100

    feat: login/register components

---
+++ src/components/register.ts
@@ -0,0 +1,77 @@
+import { createUser } from "../services/UserService";
+import { UserType } from "../types/userType";
+import { setLoginCookie } from "../components/login";
+import * as formatChecker from "../utils/formatChecker";
+import Navigo from "navigo";
+
+//Kevin's code
+export function registerTemplate() {
+  return `
+    <div class="register">
+        <h1>Register</h1>
+        <form>
+          <input type="text" id="userName" placeholder="Input username">
+          <input type="password" id="password" placeholder="Password">
+          <input type="password" id="confirmPassword" placeholder="Confirm password">
+          <button type="button" id="register">Register</button>
+          <button type="button" id="back">Back</button>
+        </form>
+    </div>
+    `;
+}
+
+//Kevin's code
+export function registerUser(router: Navigo) {
+  const userName: string = (
+    document.querySelector("#userName") as HTMLInputElement
+  ).value;
+  const password: string = (
+    document.querySelector("#password") as HTMLInputElement
+  ).value;
+  const confirmPassword: string = (
+    document.querySelector("#confirmPassword") as HTMLInputElement
+  ).value;
+
+  if (
+    formatChecker.isInputEmpty(userName) ||
+    formatChecker.isInputEmpty(password) ||
+    formatChecker.isInputEmpty(confirmPassword)
+  ) {
+    alert("Input fields cannot be empty");
+    return;
+  }
+  if (!formatChecker.isPasswordValid(password)) {
+    alert("Password must be at least 6 characters long");
+    return;
+  }
+  if (password !== confirmPassword) {
+    alert("Passwords do not match");
+    return;
+  }
+  const newUser: UserType = {
+    name: userName,
+    password: password,
+    image: "default1",
+  };
+
+  createUser(newUser).then(() => router.navigate("/"));
+  setLoginCookie(1000 * 60 * 60);
+}
+
+//Kevin's code
+export function attachRegisterEvents(router: Navigo) {
+  const registerButton = document.querySelector(
+    "#register"
+  ) as HTMLButtonElement;
+  const backButton = document.querySelector("#back") as HTMLButtonElement;
+  if (registerButton) {
+    registerButton.addEventListener("click", () => registerUser(router));
+  } else {
+    console.error("Register button not found");
+  }
+  if (backButton) {
+    backButton.addEventListener("click", () => router.navigate("/"));
+  } else {
+    console.error("Back button not found");
+  }
+}
