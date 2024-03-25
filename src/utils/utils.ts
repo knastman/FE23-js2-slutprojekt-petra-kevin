7d5a3a7c9528eb830fd7a2384923800d3b3dd7c5
Kevin <113167092+Lagergrenk@users.noreply.github.com>
2024-03-25 15:50:27 +0100

    feat: generate stuff

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,10 @@
 // Kevin's code4
 // Only for Date.now() values. example: 1647840000000
+
+import { ThreadWithId } from "../types/threadType";
+import { UserType } from "../types/userType";
+import Navigo from "navigo";
+
 // Returns date as example = "21/03/2024" and time as example "12:00:00"
 export function formatTimestamp(timestamp: number): {
   date: string;
@@ -12,4 +17,31 @@ export function formatTimestamp(timestamp: number): {
     date,
     time,
   };
+}
+
+// Kevin's code
+export function generateThreadLink(thread: ThreadWithId): HTMLAnchorElement {
+  const link = document.createElement("a");
+  link.href = `#/thread/${thread.id}`;
+  link.innerText = thread.title;
+  return link;
+}
+
+// Kevin's code
+export function generateUserLink(user: UserType): HTMLAnchorElement {
+  const link = document.createElement("a");
+  link.href = `#/user/${user.name}`;
+  link.innerText = user.name;
+  return link;
+}
+
+// Kevin's code
+export function attachLinkEvents(links: HTMLAnchorElement, router: Navigo) {
+  links.addEventListener("click", (event) => {
+    event.preventDefault();
+    const href = links.getAttribute("href");
+    if (href) {
+      router.navigate(href);
+    }
+  });
 }
