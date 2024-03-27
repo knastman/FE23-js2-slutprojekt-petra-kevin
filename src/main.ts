306136f9217050827c1408d76c61999bb3c8c73b
Kevin <113167092+Lagergrenk@users.noreply.github.com>
2024-03-27 17:21:46 +0100

    chore: testing removed

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,15 @@
-import { getTopics } from "./services/topicService.ts";
-import { displayTopicsTitles } from "./modules/displayTopics.ts";
-import { displayStartContent } from "./modules/displayStartContent.ts";
+import { getTopics } from "./services/topicService";
+import { displayTopicsTitles } from "./modules/displayTopics";
+import { displayStartContent } from "./modules/displayStartContent";
 // import { displayPageInfo } from "./modules/displayInfoPages.ts";
 import { getAllUsers, createUser } from "./services/UserService";
-import {
-  registerTemplate,
-  attachRegisterEvents,
-} from "./components/renderRegister.js";
+import { attachRegisterEvents } from "./components/renderRegister.js";
 
 ///////////////////////////////////
 
 import { setupRoutes } from "./routes/routes";
 import { renderNav } from "./components/renderNav";
 import { renderFooter } from "./components/renderFooter";
-import { renderTopicsMenu } from "./components/renderTopicsMenu.js";
 
 import Navigo from "navigo";
 
@@ -21,7 +17,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
   const router = new Navigo("/", { hash: false });
   event.preventDefault();
   renderNav(router);
-  renderTopicsMenu(router);
   setupRoutes();
   renderFooter();
 });
