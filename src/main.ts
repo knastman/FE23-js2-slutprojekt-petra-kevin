f611c2c6cf5ae6518356ace8d9c6fef76919eff5
Kevin <113167092+Lagergrenk@users.noreply.github.com>
2024-03-27 17:16:23 +0100

    chore: Checking routes with main

--- src/main.ts
+++ src/main.ts
@@ -2,26 +2,29 @@ import { getTopics } from "./services/topicService.ts";
 import { displayTopicsTitles } from "./modules/displayTopics.ts";
 import { displayStartContent } from "./modules/displayStartContent.ts";
 // import { displayPageInfo } from "./modules/displayInfoPages.ts";
-import {
-  isLoggedIn,
-  loginTemplate,
-  attachLoginEvents,
-} from "./components/login.ts";
 import { getAllUsers, createUser } from "./services/UserService";
-import { registerTemplate, attachRegisterEvents } from "./components/register";
-
+import {
+  registerTemplate,
+  attachRegisterEvents,
+} from "./components/renderRegister.js";
 
 ///////////////////////////////////
 
 import { setupRoutes } from "./routes/routes";
+import { renderNav } from "./components/renderNav";
+import { renderFooter } from "./components/renderFooter";
+import { renderTopicsMenu } from "./components/renderTopicsMenu.js";
+
+import Navigo from "navigo";
 
 document.addEventListener("DOMContentLoaded", (event) => {
+  const router = new Navigo("/", { hash: false });
   event.preventDefault();
+  renderNav(router);
+  renderTopicsMenu(router);
   setupRoutes();
+  renderFooter();
 });
-
-
-
 
 /*********************************
   Interface 
@@ -29,10 +32,5 @@ document.addEventListener("DOMContentLoaded", (event) => {
 //Petra's code
 displayStartContent();
 
-getTopics()
-.then(displayTopicsTitles); 
+getTopics().then(displayTopicsTitles);
 // .catch(displayError);
-
-
-
-
