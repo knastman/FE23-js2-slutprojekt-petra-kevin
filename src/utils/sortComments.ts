98e8d27de592f2ba55d6da18398a90279654e805
Kevin <113167092+Lagergrenk@users.noreply.github.com>
2024-03-28 14:54:44 +0100

    refactor: arraylenght can be 1

--- src/utils/sortComments.ts
+++ src/utils/sortComments.ts
@@ -3,5 +3,9 @@ import { CommentType } from "../types/commentType";
 // Kevin's code
 // Sorts comments by timestamp, oldest first
 export function sortComments(comments: CommentType[]): CommentType[] {
-  return comments.sort((a, b) => a.timeStamp - b.timeStamp);
+  if (comments.length === 1) {
+    return comments;
+  } else {
+    return comments.sort((a, b) => a.timeStamp - b.timeStamp);
+  }
 }
