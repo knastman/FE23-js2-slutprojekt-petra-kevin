543174118309e004f18c2db315d526469c31fc84
Kevin <113167092+Lagergrenk@users.noreply.github.com>
2024-03-28 16:11:15 +0100

    single

--- src/modules/displayComments.ts
+++ src/modules/displayComments.ts
@@ -1,6 +1,6 @@
-import { CommentType } from "../types/commentType";
+import { CommentType } from '../types/commentType';
 // import { UserType } from "../types/userType.ts";
-import { formatTimestamp } from "../utils/utils";
+import { formatTimestamp } from '../utils/utils';
 
 export function displayComments(thread: CommentType[]): void {
   console.log(thread);
@@ -25,9 +25,9 @@ export function displayComments(thread: CommentType[]): void {
 }
 
 export function displayComment(comment: CommentType): void {
-  const postsContainer = document.querySelector("#posts") as HTMLDivElement;
+  const postsContainer = document.querySelector('#posts') as HTMLDivElement;
   console.log(postsContainer);
-  postsContainer.classList.remove("hide");
+  postsContainer.classList.remove('hide');
 
   // console.log(comment.userName);
   // const user = comment.userName;
@@ -38,40 +38,40 @@ export function displayComment(comment: CommentType): void {
   // console.log(comment.comment);
   // console.log(comment.timeStamp);
 
-  const postBox = document.createElement("article");
-  postBox.classList.add("post");
+  const postBox = document.createElement('article');
+  postBox.classList.add('post');
 
-  const postHeader = document.createElement("div");
-  postHeader.classList.add("postHeader");
-  const postDateContainer = document.createElement("div");
-  postDateContainer.classList.add("postDate");
-  const postHeaderSubject = document.createElement("div");
-  postHeaderSubject.classList.add("postSubject");
+  const postHeader = document.createElement('div');
+  postHeader.classList.add('postHeader');
+  const postDateContainer = document.createElement('div');
+  postDateContainer.classList.add('postDate');
+  const postHeaderSubject = document.createElement('div');
+  postHeaderSubject.classList.add('postSubject');
 
-  const postBody = document.createElement("div");
-  postBody.classList.add("postBody");
+  const postBody = document.createElement('div');
+  postBody.classList.add('postBody');
 
-  const userInfo = document.createElement("div");
-  userInfo.classList.add("userInfo");
-  const userName = document.createElement("div");
-  userName.classList.add("userName");
-  const userImg = document.createElement("div");
-  const userImgSrc = document.createElement("img");
-  userImg.classList.add("userImg");
+  const userInfo = document.createElement('div');
+  userInfo.classList.add('userInfo');
+  const userName = document.createElement('div');
+  userName.classList.add('userName');
+  const userImg = document.createElement('div');
+  const userImgSrc = document.createElement('img');
+  userImg.classList.add('userImg');
 
-  const postTextDiv = document.createElement("div");
-  const postText = document.createElement("p");
-  postText.classList.add("postText");
+  const postTextDiv = document.createElement('div');
+  const postText = document.createElement('p');
+  postText.classList.add('postText');
 
-  const postFooter = document.createElement("div");
-  postFooter.classList.add("postFooter");
-  const postFooterDivleft = document.createElement("div");
-  const postTopic = document.createElement("span");
-  const postSubject = document.createElement("span");
-  const postFooterDivRight = document.createElement("div");
-  const postFooterRightEdit = document.createElement("span");
-  const postFooterRightDelete = document.createElement("span");
-  postFooter.classList.add("footerLink");
+  const postFooter = document.createElement('div');
+  postFooter.classList.add('postFooter');
+  const postFooterDivleft = document.createElement('div');
+  const postTopic = document.createElement('span');
+  const postSubject = document.createElement('span');
+  const postFooterDivRight = document.createElement('div');
+  const postFooterRightEdit = document.createElement('span');
+  const postFooterRightDelete = document.createElement('span');
+  postFooter.classList.add('footerLink');
 
   postHeaderSubject.innerText = comment.title;
 
@@ -99,9 +99,9 @@ export function displayComment(comment: CommentType): void {
   postText.innerText = comment.comment;
   // postFooterDivleft.innerText = post.topic;
   // postFooterDivleft.innerText = topics.topic;
-  postFooterDivleft.innerText = "Datorer/IT"; //Hämta dynamiskt
-  postFooterRightEdit.innerText = "Redigera inlägg";
-  postFooterRightDelete.innerText = "| Radera inlägg";
+  postFooterDivleft.innerText = 'Datorer/IT'; //Hämta dynamiskt
+  postFooterRightEdit.innerText = 'Redigera inlägg';
+  postFooterRightDelete.innerText = '| Radera inlägg';
 
   postsContainer.append(postBox);
   postBox.append(postHeader, postBody, postFooter);
